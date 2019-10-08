899a45987aaf4139c5b911db8710656fa4859e78
ulucay <clarkulucay@gmail.com>
2019-10-08 14:01:59 -0500

    add events on list items

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,9 @@
-alert("whatsup");
+// Check off specific todos by clicking
+$("li").on("click", (e) =>{
+    $(e.target).toggleClass("completed");
+})
+
+//Click on X to delete Todo
+$("span").on("click", (e)=> {
+    alert("Clicked on")
+})

c06405ff51b168e0efbbed4bee1288781b519277
ulucay <clarkulucay@gmail.com>
2019-10-10 15:00:44 -0500

    added functionality and styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,23 @@
 // Check off specific todos by clicking
-$("li").on("click", (e) =>{
+$("ul").on("click", "li", (e) => {
     $(e.target).toggleClass("completed");
-})
+});
 
 //Click on X to delete Todo
-$("span").on("click", (e)=> {
-    $(e.target).parent().fadeOut(500, () =>{
+$("ul").on("click", "span", (e) => {
+    $(e.target).parent().fadeOut(300, () =>{
         $(e.target).remove();  
     });
     e.stopPropagation();
-})
+});
+
+$("input[type='text']").on("keypress", (e) => {
+    if(e.which === 13){
+        //grab new todo text from input
+        let todoText = $(e.target).val();
+        //clear the input bar
+        $(e.target).val("");
+        //create a new li and add to ul
+        $("ul").append("<li><span>X</span> " + todoText + "</li>");
+    }
+});
